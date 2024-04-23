32337e3db2dcdb6a44499f04750a2b0183e1508d
btrizzzy <147527808+btrizzzy@users.noreply.github.com>
2024-04-22 18:15:47 -0600

    feat: add styles

--- scripts.js
+++ scripts.js
@@ -3,11 +3,7 @@ const acc = document.querySelectorAll(".accordion");
 
 acc.forEach(button => {
   button.addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
     this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
     const panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
@@ -16,6 +12,46 @@ acc.forEach(button => {
     }
   });
 });
+
+/* Blog Posts */
+// Hide all elements with class="containerTab", except for the one that matches the clickable grid column
+function openTab(tabName) {
+    var i, x;
+    x = document.getElementsByClassName("containerTab");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+}
+
+/* Countdown */
+var countdownDate = new Date("Jul 12, 2024 0:0:0").getTime();
+
+var x = setInterval(function() {
+
+var now = new Date().getTime();
+
+var distance = countdownDate - now;
+
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+const countdown = document.getElementById('countdown');
+countdown.innerHTML = days + "d " + hours + "h "
++ minutes + "m " + seconds + "s ";
+}, 1000);
+
+/* blog posts */
+function openTab(tabId) {
+    var tabs = document.getElementsByClassName("containerTab");
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = "none";
+    }
+    document.getElementById(tabId).style.display = "block";
+}
+
 
 /* Changing Copyright Year */
 const currentYear = document.getElementById('currentyear');
